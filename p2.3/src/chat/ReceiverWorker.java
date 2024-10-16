f477a71f3495977e483d7c8be205c34137b54073
Nick Wiley <nwiley333@gmail.com>
2024-10-15 18:21:50 -0700

    ReceiverWorker finished

--- p2.3/src/chat/ReceiverWorker.java
+++ p2.3/src/chat/ReceiverWorker.java
@@ -5,11 +5,15 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import org.w3c.dom.Node;
 
 import message.Message;
 import message.MessageTypes;
@@ -17,6 +21,7 @@ import message.MessageTypes;
 
 public class ReceiverWorker extends Thread {
     Socket serverConnection = null;
+    Socket nodeConnection = null;
 
     ObjectInputStream readFromNet = null;
     ObjectOutputStream writeToNet = null;
@@ -76,7 +81,7 @@ public class ReceiverWorker extends Thread {
             case MessageTypes.LEAVE:
                 // remove this participant's info
                 NodeInfo leavingParticipantInfo = (NodeInfo) message.getContent();
-                if (ChatServer.participants.remove(leavingParticipantInfo))
+                if (ChatClient.participants.remove(leavingParticipantInfo))
                 {
                     System.err.println(leavingParticipantInfo.getName() + " removed");
                 }
@@ -101,16 +106,39 @@ public class ReceiverWorker extends Thread {
             
             case MessageTypes.JOIN:
                 // read participant's NodeInfo
-                NodeInfo joiningParticipantNodeInfo = (NodeInfo) message.getContent();
+                participantInfo = (NodeInfo) message.getContent();
+
+                // Send INFO message back
+                try
+                {
+                    // open socket to one chat client at a time
+                    nodeConnection = new Socket(participantInfo.address, participantInfo.port);
+
+                    // open object streams
+                    writeToNet = new ObjectOutputStream(nodeConnection.getOutputStream());
+                    readFromNet = new ObjectInputStream(nodeConnection.getInputStream());
+
+                    // Write INFO message
+                    writeToNet.writeObject(new Message(MessageTypes.INFO, ChatClient.participants));
+
+                    // close node connection
+                    nodeConnection.close();
+                }
+                catch (Exception err)
+                {
+                    Logger.getLogger(ReceiverWorker.class.getName()).log(Level.SEVERE, 
+                                        "[ReceiverWorker].run Could not open socket to node or send a message", 
+                                        err);
+                }
 
                 // add this client to list of participants
-                ChatServer.participants.add(joiningParticipantNodeInfo);
+                ChatClient.participants.add(participantInfo);
 
                 // show who joined
-                System.out.print(joiningParticipantNodeInfo.getName() + " joined. All current participants: ");
+                System.out.print(participantInfo.getName() + " joined. All current participants: ");
 
                 // print out all current participants
-                participantsIterator = ChatServer.participants.iterator();
+                participantsIterator = ChatClient.participants.iterator();
                 while(participantsIterator.hasNext())
                 {
                     participantInfo = participantsIterator.next();
@@ -121,9 +149,42 @@ public class ReceiverWorker extends Thread {
                 break;
             
             case MessageTypes.JOINED:
+                // read participant's NodeInfo
+                participantInfo = (NodeInfo) message.getContent();
+
+                // add new node to participants list
+                ChatClient.participants.add(participantInfo);
+
+                // print out newly joined node
+                System.out.println(participantInfo.getName() + " joined. All current participants: ");
+
+                // printing out all participants
+                participantsIterator = ChatClient.participants.iterator();
+                while(participantsIterator.hasNext())
+                {
+                    participantInfo = participantsIterator.next();
+                    System.out.print(participantInfo.getName() + " ");
+                }
+                System.out.println();
+
                 break;
             
             case MessageTypes.INFO:
+                // init list for received participants
+                List<NodeInfo> newParticipants = (List<NodeInfo>) message.getContent();
+
+                // loop through all received participants and add to participants
+                participantsIterator = newParticipants.iterator();
+                while(participantsIterator.hasNext())
+                {
+                    // get next participant
+                    participantInfo = participantsIterator.next();
+                    // add new participant to participants list
+                    ChatClient.participants.add(participantInfo);
+                }
+                // add original node to participants list
+                ChatClient.participants.add(ChatClient.knownNodeInfo);
+                
                 break;
             default:
         }
