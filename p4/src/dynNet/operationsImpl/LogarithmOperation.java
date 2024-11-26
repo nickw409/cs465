d990ac17927c0e6a263be092259df4582b6a0b48
AlanHakalaDev <123601775+AlanHakalaDev@users.noreply.github.com>
2024-11-26 15:27:40 -0700

    Update LogarithmOperation.java

--- p4/src/dynNet/operationsImpl/LogarithmOperation.java
+++ p4/src/dynNet/operationsImpl/LogarithmOperation.java
@@ -2,9 +2,9 @@ package dynNet.operationsImpl;
 
 import dynNet.dynCalculator.Operation;
 
-public class RootOperation {
+public class LogarithmOperation {
     public float calculate(float firstNumber, float secondNumber)
     {
-        return (float)Math.pow(firstNumber, (1.0 / secondNumber));
-    }
+        return (float)(Math.log(firstNumber) / Math.log(secondNumber));
+    }    
 }
